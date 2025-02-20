3feedcf857fbf215f79a06e0c2a086a83aba11c5
Don Undeen <donundeen@gmail.com>
2025-02-20 15:56:05 -0500

    new notes

--- TODO.md
+++ TODO.md
@@ -4,13 +4,17 @@
 - addtl skill tree columns
 -- link to documentation
 -- "admin notes" for us - what are the skill tree items that we should focus on?
--- each skill tree item needs an ID - base it on the Description
+-- each skill tree item needs an ID - base it on the Title
 - for each skill tree item, auto-create a Google Slide Deck where we will store the skill tree item how-tos and other documentation
 - make a tracker that can show the last update time for each skill tree item document
 
+- Google Sheets CRUD operations
+-- create
+-- read
+-- update
+-- delete
 
-- call a function on the server side from the client via a onclick event - DONE
-- parse a json object into a google sheet - DONE
+
 
 
 # THINGS I LEARNED
--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import getEmailAddress from './email';
 //import jsonToSpreadsheet from './jsontospreadhsheet';
-import {testButtonClicked, testButtonClicked3, jsonToSpreadsheet} from './clientCallableFunctions';
+import {testButtonClicked, testButtonClicked3, jsonToSpreadsheet, listJsonFiles, getAllSkillTreeRows} from './clientCallableFunctions';
+import {dataIntoHashRows, updateHashRow, insertHashRow, getSheetRows} from './crudOperations';  
 
 global.getEmailAddress = getEmailAddress;
 global.jsonToSpreadsheet = jsonToSpreadsheet;
 global.testButtonClicked = testButtonClicked;
 global.testButtonClicked3 = testButtonClicked3;
-/*
-global.doGet = (resE) => {
-    const text = '<b>I heart Apps Script...</b>';
-    const output = HtmlService.createHtmlOutput(text);
-    output.setTitle(text);
-    output.addMetaTag('viewport', 'width=device-width, initial-scale=1');
-    output.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-    return output;
-};
-*/
-
+global.listJsonFiles = listJsonFiles;
+global.dataIntoHashRows = dataIntoHashRows;
+global.updateHashRow = updateHashRow;
+global.insertHashRow = insertHashRow;
+global.getSheetRows = getSheetRows;
+global.getAllSkillTreeRows = getAllSkillTreeRows;
 
 global.doGet = (e) => {
     Logger.log("opening");  
@@ -38,9 +34,15 @@ global.include = (filename) => {
   
 global.testJsonToSpreadsheet = function(){
   let spreadsheetID = "12GfSYyx1oIm2V-ZpDOqA7wBXeEbXLjihPP8Txem5J5Q";
-  let sheetName = "SkillTrees";
+  let sheetName = "3D Printing and Modeling";
   let filename = "3D_Printing.json";
-  let result =  jsonToSpreadsheet(filename, sheetName, spreadsheetID);
+  let result =  jsonToSpreadsheet(filename, spreadsheetID,sheetName );
+  Logger.log(result);
+  return result;
+}
+
+global.currentTestFunction = function(){
+  let result = global.getAllSkillTreeRows();
   Logger.log(result);
   return result;
 }
@@ -53,188 +55,7 @@ let res = ImgApp.doResize(file.getId(), newWidth);
     imageFolder.createFile(res.blob.setName(thumbname));  
 */
 }
-global.listJsonFiles = function() {
-  Logger.log("listing json files");
-  const files = DriveApp.getFilesByType('application/json');
-  while (files.hasNext()) {
-      const file = files.next();
-      Logger.log('File Name: ' + file.getName() + ', File ID: ' + file.getId());
-  }
-}
-  
-  
-  function dataIntoHashRows(data, keysRow, startRow, filterFunction){
-    let idKey= {};
-    let keyId= {};
-    let newData = [];
-    Logger.log("data");
-  
-    for (let k = 0; k < data[keysRow].length; k++) { 
-      // key is text, k is number
-  
-      let key = data[keysRow][k];
-      key = key.replace("?","");
-      key = key.replace("'","");
-      key = key.replace(":","");
-      if(key.trim() === ""){
-         continue;
-      }
-      
-      idKey[k] = key;
-      keyId[key] = k;
-    }
-      
-    for (let i = startRow; i < data.length; i++) { 
-      let newRow = {};
-      for (let j = 0; j < data[i].length; j++) { 
-        if(!idKey[j] || idKey[j].trim() === ""){
-          continue; 
-        }
-        newRow[idKey[j]] = data[i][j];
-      }
-      newRow._Sheet_Row_ID = i;
-      if(!filterFunction || filterFunction(newRow) === true){
-        newData.push(newRow);
-      }
-    }
-    
-    return {data:newData, keyId: keyId, idKey: idKey};
-    
-  }
-  
 
-  
-  /* 
-  ============================ UPDATEHASHROW =================================
-  update a row in a sheet. Use column names as keys.  
-  table: the google sheets object
-  data: the row, with column names as keys. Missing columns will be updated to 
-  blank, NOT left alone.
-  keysrow: which row of the table holds the column names (starts a 0, NOT 1)
-  updateKey: object {key: column Name of identifying key of row to update 
-  (eg 'NetId'), value : value for that cell in that row (eg 'dhu3')
-  ============================================================================
-  */
-  
-  
-  function updateHashRow(table, data, keysrow, updateKeys){
-  
-    let to_continue = _updateHashRow(table, data, keysrow, updateKeys);
-    
-  }
-
-  function _updateHashRow(table, data, keysrow, updateKeys){
-    Logger.log("updating2");
-    let insertArray = [];
-    let idKey= {};
-    let keyId= {};
-      
-    let range = "A"+(keysrow+1).toString() +":"+(keysrow+1).toString();
-  
-    let tableMetaData = table
-    .getActiveSheet()
-    .getRange(range)
-    .getValues();  
-    
-    for (let k = 0; k < tableMetaData[0].length; k++) { 
-      let key = tableMetaData[0][k];
-      // key is text, k is number
-      if(key.trim() === ""){
-         continue;
-      }
-      insertArray.push(""); 
-      idKey[k] = key;
-      keyId[key] = k;
-    }
-     
-    let datakeys = Object.keys(data);
-  
-    for(let i = 0; i < datakeys.length; i++){
-      let key = datakeys[i];
-      let k = keyId[key];
-      insertArray[k] = data[key];
-    }
-    
-    let index = findRowNumForQuery(table, keysrow, keysrow + 1, function(row){
-      Logger.log("updateKeys");
-      let updateKeysKeys = Object.keys(updateKeys);
-      Logger.log(updateKeysKeys);
-      for(let i = 0; i < updateKeysKeys.length; i++){
-        let key = updateKeysKeys[i];
-        let value = updateKeys[key];      
-        Logger.log(key + " : " + value + " : " + row[key]);
-        if(row[key] !== value){
-          return false;
-        }
-      }
-      return true;
-    });
-    
-    Logger.log("returning ", index);
-    if(!index){
-      return false; 
-    }
-    let toDelete = index + 1;
-    
-    if(index){
-      table.getActiveSheet().deleteRow(toDelete);
-    }
-    table.getActiveSheet().appendRow(insertArray);
-    
-    return index;
-    
-  }
-  
-  
-  function findRowNumForQuery(table, keysRow, startRow, queryFunction){
-    let tableData = table.getActiveSheet().getDataRange().getValues();
-  
-    let data = dataIntoHashRows(tableData, keysRow, startRow).data;
-      
-    for (let i = 0; i < data.length; i++) { 
-      let res = queryFunction(data[i]);
-      if(res === true){
-        return i + startRow;
-      }
-    }
-    return false;
-  }
-  
-  
-  function insertHashRow(table, data, keysrow){
-    let insertArray = [];
-    let idKey= {};
-    let keyId= {};
-      
-    let range = "A"+(keysrow+1).toString() +":"+(keysrow+1).toString();
-  
-    let tableMetaData = table
-    .getActiveSheet()
-    .getRange(range)
-    .getValues();  
-    
-    for (let k = 0; k < tableMetaData[0].length; k++) { 
-      let key = tableMetaData[0][k];
-      // key is text, k is number
-      if(key.trim() === ""){
-         continue;
-      }
-      insertArray.push(""); 
-      idKey[k] = key;
-      keyId[key] = k;
-    }
-     
-    let datakeys = Object.keys(data);
-  
-    for(let i = 0; i < datakeys.length; i++){
-      let key = datakeys[i];
-      let k = keyId[key];
-      insertArray[k] = data[key];
-    }
-    
-    table.getActiveSheet().appendRow(insertArray);
-  }
-  
   
   function getFileBlob(filename){
     // https://developers.google.com/apps-script/reference/base/blob
